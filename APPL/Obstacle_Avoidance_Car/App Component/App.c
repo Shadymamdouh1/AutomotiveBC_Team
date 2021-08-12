c87f253e5bdad97cebcdb70fa45e9fa0f156c76e
AhmedNabil <ahmednabil.gad18@gmail.com>
2021-08-12 18:56:54 +0200

    Working Empty Project + Modules Included Ultrasonic + ICU + LCD + Steering Modules Remaining

--- APPL/Obstacle_Avoidance_Car/App Component/App.c
+++ APPL/Obstacle_Avoidance_Car/App Component/App.c
@@ -38,7 +38,7 @@ enuApp_Status_t RobotApp_start(void)
 	EnableGlobalInterrupts();
 	
 	
-	Robot_move(ROBOT_DIR_FRWRD, 70);
+	Robot_move(ROBOT_DIR_FRWRD, 20);
 	/* Application Super Loop */
 	while (1)
 	{
