41fe8e7dd56ad1b073fb6545ee97164235c58e15
AhmedNabil <ahmednabil.gad18@gmail.com>
2021-08-12 20:06:25 +0200

    Steering Module Added

---
+++ APPL/Obstacle_Avoidance_Car/Robot Steering Module/Rbt_Steering_Cfg.c
@@ -0,0 +1,16 @@
+/*
+ * Robot_Cfg.c
+ *
+ * Created: 9/8/2021 11:17:06 AM
+ *  Author: Ahmed Nabil
+ */ 
+
+#include "Steering.h"
+
+/*
+ * Array of Structures, each holds the two motors of the robot left and right
+ */
+const strRobot_Config_t str_RobotsConfig =
+{
+	MOTOR_LEFT_ID, MOTOR_RIGHT_ID
+};
