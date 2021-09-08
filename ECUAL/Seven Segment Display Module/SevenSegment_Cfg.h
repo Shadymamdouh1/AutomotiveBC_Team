fb3159ced6bdcbac8c0076c6aef33c70a114c6c2
Mohamed Magdy <mohamedmagdy.elkholy@gmail.com>
2021-09-08 22:16:30 +0200

    Added Seven Segment Module V1.0

---
+++ ECUAL/Seven Segment Display Module/SevenSegment_Cfg.h
@@ -0,0 +1,20 @@
+/*
+ * SevenSegment_Cfg.h
+ *
+ * Created: 08-Sep-21 6:41:34 PM
+ *  Author: Mohamed Magdy
+ */ 
+
+
+#ifndef SEVENSEGMENT_CFG_H_
+#define SEVENSEGMENT_CFG_H_
+
+#define NUM_7SEG_USED		(2U)
+
+#define SEG_1_ID			(0U)
+#define SEG_2_ID			(1U)
+
+
+
+
+#endif /* SEVENSEGMENT_CFG_H_ */
