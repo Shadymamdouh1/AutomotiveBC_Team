9cd38fb959dc97298f5ab9d0dfc305ef88d9333d
AhmedNabil <ahmednabil.gad18@gmail.com>
2021-09-08 13:51:12 +0200

    Added functions in DIO and GPT

--- MCAL/Gpt Module/Gpt_LCfg.c
+++ MCAL/Gpt Module/Gpt_LCfg.c
@@ -18,8 +18,8 @@
 strGpt_Config_t strGpt_Channels[GPT_USED_CHANNELS]=
 {
 	/*Timer Number*/ /*Prescaler*/   /* Mode */
-	{  TIMER_0    ,   PRESCALER_64  , NORMAL_MODE   },
-	{  TIMER_1    ,   PRESCALER_8  , NORMAL_MODE   },
-	{  TIMER_2    ,   PRESCALER_64  , NORMAL_MODE   }
+	{  TIMER_0    ,   PRESCALER_1024  , NORMAL_MODE   }
+// 	{  TIMER_1    ,   PRESCALER_8  , NORMAL_MODE   },
+// 	{  TIMER_2    ,   PRESCALER_64  , NORMAL_MODE   }
 	
 };
